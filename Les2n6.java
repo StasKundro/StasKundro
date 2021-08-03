8943590a2157d2b06a35def15ed04b7b648ff9f1
Stas <88132419+StasKundro@users.noreply.github.com>
2021-08-03 11:36:37 +0300

    Add files via upload

---
+++ Les2n6.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Les2n6 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 101) {
+            i++;
+            if ((i % 2) == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
