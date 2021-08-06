cc6762efb8402ce07d343b8605d1a80447a46744
Stas <88132419+StasKundro@users.noreply.github.com>
2021-08-06 18:53:12 +0300

    Add files via upload

---
+++ LessonOneFirst.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class LessonOneFirst {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+    }
+}
---
+++ LessonOneSecond.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class LessonOneSecond {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, team");
+    }
+}
